e6b0c4c5b903253d7ebafe73e4a76fc3dcc40a40
gpcprado <gpcprado@gbox.ncf.edu.ph>
2025-12-06 19:14:52 +0800

    Update concern.service.ts

--- src/concern/concern.service.ts
+++ src/concern/concern.service.ts
@@ -4,7 +4,7 @@ import { RowDataPacket, OkPacket } from 'mysql2';
 
 export interface Concern {
   concern_id?: number; 
-  username: string; 
+  usernameCon: string; 
   concern_content: string; 
   id: number; 
   sentTime?: Date | string;
@@ -36,15 +36,15 @@ export class ConcernService {
     }
   }
 
-  async createConcern(username: string, concern_content: string, id: number) { 
+  async createConcern(usernameCon: string, concern_content: string, id: number) { 
     const [result] = await this.pool().execute<OkPacket>(
-      'INSERT INTO concern (username, concern_content, id) VALUES (?, ?, ?)',
-      [username, concern_content, id ?? null]
+      'INSERT INTO concern (usernameCon, concern_content, id) VALUES (?, ?, ?)',
+      [usernameCon, concern_content, id ?? null]
     );
     
     return { 
       concern_id: (result as any).insertId,
-      username: username, 
+      usernameCon: usernameCon, 
       concern_content: concern_content,
       id: id,
     } as Concern;
@@ -52,24 +52,24 @@ export class ConcernService {
 
   async findById(userId: number) {
     const [rows] = await this.pool().execute<RowDataPacket[]>(
-      'SELECT id, username, concern_content FROM concern WHERE id = ?', 
+      'SELECT id, usernameCon, concern_content FROM concern WHERE id = ?', 
       [userId],
     );
     return rows[0];
   }
 
-  async updateConcern(concern_id: number, data: { username?: string; concern_content?: string }) { 
-    const { username, concern_content } = data; 
+  async updateConcern(concern_id: number, data: { usernameCon?: string; concern_content?: string }) { 
+    const { usernameCon, concern_content } = data; 
 
     const [result]: any = await this.pool().execute(
-      'UPDATE concern SET username = ?, concern_content = ? WHERE concern_id = ?',
-      [username, concern_content, concern_id] 
+      'UPDATE concern SET usernameCon = ?, concern_content = ? WHERE concern_id = ?',
+      [usernameCon, concern_content, concern_id] 
     );
 
     if (result.affectedRows === 0) {
       throw new NotFoundException(`Message with ID ${concern_id} not found`); 
     }
 
-    return { concern_id, username, concern_content } as Concern; 
+    return { concern_id, usernameCon, concern_content } as Concern; 
   }
 }
